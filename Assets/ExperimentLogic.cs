58da863fe46dd97ec751e5f1d305538c67645d60
Li Alex Zhang <fff008@gmail.com>
2016-02-06 17:47:17 -0800

    first commit

---
+++ Assets/ExperimentLogic.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Diagnostics;
+
+public class Subject
+{
+    public string name;
+    public string id;
+    public string species;
+    public string gender;
+    public float age;
+    public Vector3 size;
+    public float weight;
+}
+
+public struct Control
+{
+    public int blockcount;
+    public int trialcount;
+    public int conditiontestcount;
+}
+
+public class Experiment
+{
+    public string name;
+    public string id;
+    public Subject subject=new Subject();
+    public List<Dictionary<string, object>> condition;
+    public double preIBI, blockdur, sufIBI, preITI, trialdur, sufITI, preICI, conditiontestdur, sufICI;
+    public int nblock, ntrial, nconditiontest;
+    public Control control;
+    public Timer timer=new Timer();
+}
+
+public class Timer : Stopwatch
+{
+    public double ElapsedSeconds
+    {
+        get { return Elapsed.TotalSeconds; }
+    }
+
+    public void Rest(double restT)
+    {
+        if (!IsRunning)
+        {
+            Start();
+        }
+        double startT, endT;
+        startT = Elapsed.TotalSeconds;
+        endT = Elapsed.TotalSeconds;
+        while ((endT - startT) < restT)
+        {
+            endT = Elapsed.TotalSeconds;
+        }
+    }
+
+    public void ReStart()
+    {
+        Reset();
+        Start();
+    }
+}
+
+public class ExperimentLogic : MonoBehaviour
+{
+    public Experiment experiment = new Experiment();
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/exlogictest.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class exlogictest : ExperimentLogic {
+    double ontime = 0;
+	// Use this for initialization
+	void Start () {
+        experiment.trialdur = 0.05;
+        experiment.sufITI = 0.05;
+        ontime = experiment.timer.ElapsedSeconds;
+        
+        experiment.timer.Start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var player = GameObject.Find("Player");
+        if (player)
+        {
+            if (player.activeInHierarchy)
+            {
+                player.SetActive(false);
+            }
+        }
+        var go = GameObject.Find("Quad");
+        var cp = go.GetComponent<Quad>();
+        var qs = cp.GetComponent<NetSyncBase>();
+
+
+        if (!experiment.timer.IsRunning)
+        {
+            experiment.timer.Start();
+        }
+        if ((experiment.timer.ElapsedSeconds-ontime)>experiment.trialdur)
+        {
+            if(qs)
+            {
+                var p = qs.transform.position;
+                p.x = (Random.value * 2 - 1) * 10;
+                p.y = (Random.value * 2 - 1) * 10;
+                qs.position = p;
+                ontime = experiment.timer.ElapsedSeconds;
+            }
+        }
+	}
+}
