798eb76a979f8848f8afe5106d549800e6c354d1
Li Alex Zhang <fff008@gmail.com>
2019-10-26 16:39:31 -0700

    add hueangle in colordata

--- ColorLab/Generate_ColorData.jl
+++ ColorLab/Generate_ColorData.jl
@@ -61,9 +61,8 @@ minc_dkl = quavectors(th.-0.5*DKLIsoRGBVec);maxc_dkl = quavectors(th.+0.5*DKLIso
 # DKL Isoluminance plane
 DKLToRGB = LMSToRGB*DKLToLMS
 RGBToDKL = LMSToDKL*RGBToLMS
-lum = 0;hueangle = 30
+lum = 0;hueangle = 30;hueangles = 0:hueangle:180-hueangle
 hueangles = [0,30,60,75,80,83,90,95,101,105,110,120,150]
-hueangles = 0:hueangle:180-hueangle
 # rotate l-m direction around l+m axis within the Isoluminance plane
 DKLIsoLumRGBVec = trivectors(hcat(map(i->DKLToRGB*RotateXYZMatrix(deg2rad(i),dims=1)*[0,1,0,0], hueangles)...))
 # find Intersections of Isoluminance directions with faces of unit RGB cube
@@ -71,6 +70,7 @@ minmaxc = hcat(map(i->intersectlineunitorigincube((DKLToRGB*[lum,0,0,1])[1:3],DK
 minc_dkl_ilp=minmaxc[:,1:2:end];maxc_dkl_ilp=minmaxc[:,2:2:end]
 hue_dkl_ilp = quavectors([maxc_dkl_ilp minc_dkl_ilp])
 wp_dkl_ilp = repeat([th;1],inner=(1,size(hue_dkl_ilp,2)))
+hueangle_dkl_ilp = [hueangles;hueangles.+180]
 
 # Check Hues
 IsoLumc = [RGB(hue_dkl_ilp[1:3,i]...) for i in 1:size(hue_dkl_ilp,2)]
@@ -81,14 +81,13 @@ p
 
 
 # HSL equal angular distance hue[0:30:330] and equal energy white with matched luminance
-hslhuewp = copy(hslhuewpYmatch)
-hslhuewp[3,:].= 0.4
+hslhuewp = copy(hslhuewpmY)
+hslhuewp[3,:] .= 0.4
 huewp_hsl=XYZToRGB*quavectors(xyY2XYZ(hslhuewp))
 huewphslc = [RGB(huewp_hsl[1:3,i]...) for i in 1:size(huewp_hsl,2)]
 hue_hsl = huewp_hsl[:,1:end-1]
 wp_hsl = repeat(huewp_hsl[:,end],inner=(1,size(hue_hsl,2)))
-
-
+hueangle_hsl = 0:30:330
 
 
 
@@ -131,16 +130,18 @@ RGBToLMS*[th;1]
 colordata = Dict("LMS_X"=>colorstring.([maxc_lms[:,1],minc_lms[:,1]]),
                 "LMS_Y"=>colorstring.([maxc_lms[:,2],minc_lms[:,2]]),
                 "LMS_Z"=>colorstring.([maxc_lms[:,3],minc_lms[:,3]]),
-                #"LMS_XYZ_MichelsonContrast" => colorstring(diag(mcc)),
-                "LMS_Xm"=>colorstring.([mmaxc_lms[:,1],mminc_lms[:,1]]),
-                "LMS_Ym"=>colorstring.([mmaxc_lms[:,2],mminc_lms[:,2]]),
-                "LMS_Zm"=>colorstring.([mmaxc_lms[:,3],mminc_lms[:,3]]),
-                #"LMS_XYZm_MichelsonContrast" => colorstring(diag(mmcc)),
+                "LMS_XYZ_MichelsonContrast" => diag(mcc),
+                "LMS_mX"=>colorstring.([mmaxc_lms[:,1],mminc_lms[:,1]]),
+                "LMS_mY"=>colorstring.([mmaxc_lms[:,2],mminc_lms[:,2]]),
+                "LMS_mZ"=>colorstring.([mmaxc_lms[:,3],mminc_lms[:,3]]),
+                "LMS_mXYZ_MichelsonContrast" => diag(mmcc),
                 "DKL_X"=>colorstring.([maxc_dkl[:,1],minc_dkl[:,1]]),
                 "DKL_Y"=>colorstring.([maxc_dkl[:,2],minc_dkl[:,2]]),
                 "DKL_Z"=>colorstring.([maxc_dkl[:,3],minc_dkl[:,3]]),
-                "HSL_HueYmatch" => colorstring.([hue_hsl_Ymatch[:,i] for i in 1:size(hue_hsl_Ymatch,2)]),
-                "HSL_WPYmatch" => colorstring.([wp_hsl[:,i] for i in 1:size(wp_hsl,2)]),
+                "HSL_HueAngle_mY" => hueangle_hsl,
+                "HSL_Hue_mY" => colorstring.([hue_hsl[:,i] for i in 1:size(hue_hsl,2)]),
+                "HSL_WP_mY" => colorstring.([wp_hsl[:,i] for i in 1:size(wp_hsl,2)]),
+                "DKL_HueAngle_0"=>hueangle_dkl_ilp,
                 "DKL_HueMax_0"=>colorstring.([hue_dkl_ilp[:,i] for i in 1:size(hue_dkl_ilp,2)]),
                 "DKL_WP_0"=>colorstring.([wp_dkl_ilp[:,i] for i in 1:size(wp_dkl_ilp,2)]))
 
@@ -150,6 +151,13 @@ open(resultpath,"w") do f
 end
 
 
+YAML.write_file(resultpath,colordata)
+
+a=RGBToXYZ*([0,23,0,255]/255)
+
+
+
+XYZ2xyY(a)
 
 
 
@@ -158,7 +166,7 @@ end
 
 using MAT
 a=matread("hueTest.mat")
-a=a["test1"]
+a=a["test"]
 
 IsoLumc = [RGB(a[1:3,i]...) for i in 1:size(a,2)]
 
