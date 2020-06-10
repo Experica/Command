75d20bc34479935f85ce2a77beca3282e43af399
Li Alex Zhang <fff008@gmail.com>
2020-06-10 11:15:48 -0700

    update

--- Condition/GenerateCondition.jl
+++ Condition/GenerateCondition.jl
@@ -5,17 +5,18 @@ cd(@__DIR__)
 dk=0.2
 kbegin=0.2
 kend=6.6
+# add 4 uniform gray as blanks to match 4 phases of each grating with the same mean luminance
+nblank=4
 
-# add 4 uniform gray to match 4 phases of each grating with same average luminance
-hs = hartleysubspace(kbegin=kbegin,kend=kend,dk=dk,phase=0,shape=:circle,addhalfcycle=true,blank=(kx=0,ky=0,phase=0.375),nblank=4)
+hs = hartleysubspace(kbegin=kbegin,kend=kend,dk=dk,phase=0.0,shape=:circle,addhalfcycle=true,blank=(kx=0.0,ky=0.0,phase=0.375),nblank=nblank)
 ss = map(i->cas2sin(i...),hs)
 
 # check gratings
 @manipulate for i in 1:length(ss)
-    Gray.(grating(θ=ss[i].θ,sf=ss[i].f,phase=ss[i].phase,sized=(5,5),ppd=30))
+    Gray.(grating(θ=ss[i].θ,sf=ss[i].f,phase=ss[i].phase,size=(5,5),ppd=30))
 end
 
 # save condition
 hartleyconditions = Dict(:Ori=>map(i->i.θ,ss),:SpatialFreq=>map(i->i.f,ss),:SpatialPhase=>map(i->i.phase,ss))
-title = "Hartley_k[$kbegin,$kend]_dk[$dk]"
+title = "Hartley_k[$kbegin,$kend]_dk[$dk]_nblank[$nblank]"
 YAML.write_file("$title.yaml",hartleyconditions)
